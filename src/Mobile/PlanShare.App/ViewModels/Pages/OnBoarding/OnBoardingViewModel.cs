2c617b5a00024e79b535fc2085e94bdb26bb2a11
Reinan Guilherme <r.guilhermedeoliveira3@gmail.com>
2025-06-28 15:26:48 -0300

    Adicionando comando em elementos que não são botões

--- src/Mobile/PlanShare.App/ViewModels/Pages/OnBoarding/OnBoardingViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/OnBoarding/OnBoardingViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using PlanShare.App.Navigation;
 
 namespace PlanShare.App.ViewModels.Pages.OnBoarding;
 
@@ -7,12 +8,18 @@ public partial class OnBoardingViewModel
     [RelayCommand]
     public async Task LoginWithEmailAndPassword()
     {
-        await Shell.Current.GoToAsync("DoLoginPage");
+        await Shell.Current.GoToAsync(RoutePages.LOGIN_PAGE);
     }
 
     [RelayCommand]
     public void LoginWithGoogle()
     {
         // Logic for logging in with email and password
+    }
+
+    [RelayCommand]
+    public async Task RegisterUserAccount()
+    {
+        await Shell.Current.GoToAsync(RoutePages.USER_REGISTER_ACCOUNT_PAGE);
     }
 }
