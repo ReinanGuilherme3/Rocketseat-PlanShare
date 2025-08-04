277d532e9cea19bd25b70d697ba019a016c8c37c
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.com.br>
2025-08-04 16:09:46 -0300

    Cores para o cursor e a linha da entry

--- src/Mobile/PlanShare.App/Resources/Styles/Handlers/CustomEntryHandler.cs
+++ src/Mobile/PlanShare.App/Resources/Styles/Handlers/CustomEntryHandler.cs
@@ -1,11 +1,16 @@
-﻿namespace PlanShare.App.Resources.Styles.Handlers;
+﻿using PlanShare.App.Extensions;
+
+namespace PlanShare.App.Resources.Styles.Handlers;
 public class CustomEntryHandler
 {
     public static void Customize()
     {
         Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("PlanShareEntry", (handler, view) =>
         {
+            var cursorColor = Application.Current!.GetPrimaryColor();
+            var lineColor = Application.Current!.GetLineColor();
 
+            var hexadecimal = cursorColor.ToHex();
         });
     }
 }
