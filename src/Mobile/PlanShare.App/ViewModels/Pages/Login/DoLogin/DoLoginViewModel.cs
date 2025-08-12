41e2853b3b4ec05c842ee3975945835dced49a43
reinan.neto <reinan.neto@grupofasitec.com.br>
2025-08-12 09:58:43 -0300

    Conectando o model com a entry do nosso template

--- src/Mobile/PlanShare.App/ViewModels/Pages/Login/DoLogin/DoLoginViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/Login/DoLogin/DoLoginViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using PlanShare.App.Models;
 
 namespace PlanShare.App.ViewModels.Pages.Login.DoLogin;
 public partial class DoLoginViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public string texto;
+    public LoginModel model;
+
+    public DoLoginViewModel()
+    {
+        Model = new();
+    }
 
     [RelayCommand]
     public async Task DoLogin()
     {
-        var textoDigitado = Texto;
+        var textoDigitado = Model;
     }
 }
